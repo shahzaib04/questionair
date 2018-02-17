1b5ea560bac3d97dc982acdb1729d375e928a8e3
shahzaib04 <shahzaib.ch7.sc@gmail.com>
2018-02-17 19:04:04 +0500

    chnaged the app name

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Questionairs') }}</title>
+    <title>{{ config('app.name', 'Questionnaire') }}</title>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
