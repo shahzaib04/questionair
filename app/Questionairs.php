8fe3b84c71d23c312998d937d96d19c3adeec5e7
shahzaib04 <shahzaib.ch7.sc@gmail.com>
2018-02-19 10:52:56 +0500

    Update Questionairs.php
    Changed to the pure Eloquent

--- app/Questionairs.php
+++ app/Questionairs.php
@@ -9,19 +9,4 @@ class Questionairs extends Model {
 
     protected $table = 'questionairs';
 
-    public static function show($id) {
-        return DB::table('questionairs')->where('id', $id)
-                        ->first();
-    }
-
-    public static function getAll() {
-        return DB::table('questionairs')
-                        ->get();
-    }
-
-    public static function deleteqQuestionair($id) {
-
-        return DB::table('questionairs')->where('id', '=', $id)->delete();
-    }
-
 }
